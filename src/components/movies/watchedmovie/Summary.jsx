f3cb60abea9fcacdfd00024542ddbd2814e60bad
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-08-14 17:13:00 +0700

    feat: add movies content

---
+++ src/components/movies/watchedmovie/Summary.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Summary = () => {
+    return (
+        <>
+            <section className='p-8 flex flex-col gap-4 shadow-[0px_20px_33px_-8px_rgba(0,0,0,0.49)]'>
+                <h3 className='text-xl text-slate-200 font-semibold'>MOVIES YOU WATCHED</h3>
+                <div className='flex gap-6'>
+                    <p>
+                        <span>#️⃣ </span>
+                        <span className='text-slate-200 text-sm'>0 Movies</span>
+                    </p>
+                    <p>
+                        <span>🎬 </span>
+                        <span className='text-slate-200 text-sm'>0.0</span>
+                    </p>
+                    <p>
+                        <span>🌟 </span>
+                        <span className='text-slate-200 text-sm'>0.0</span>
+                    </p>
+                    <p>
+                        <span>⏳ </span>
+                        <span className='text-slate-200 text-sm'>0.0</span>
+                    </p>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Summary;
--- src/components/navbar/Search.jsx
+++ src/components/navbar/Search.jsx
@@ -10,7 +10,7 @@ const Search = () => {
                     className='w-96 rounded-lg bg-sidebar border-sidebar text-slate-400 p-4 pe-12 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent'
                     type='text'
                 />
-                <Button>Search</Button>
+                <Button className={'px-4 py-2 bg-primary text-slate-200 rounded-xl'}>Search</Button>
             </form>
         </>
     );
